4a93a6ca662dd2ec5fb1a8ded5a0af5b50dcfb15
Aaron Jumarang <kapj.jumarang@gmail.com>
2019-01-17 09:28:42 -0500

    Added line following class

---
+++ src/main/java/frc/utils/LineFollower.java
@@ -0,0 +1,11 @@
+package frc.utils;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+
+public class LineFollower
+{
+    public LineFollower()
+    {
+        DigitalInput Sensor = new DigitalInput(1);
+    }
+}
