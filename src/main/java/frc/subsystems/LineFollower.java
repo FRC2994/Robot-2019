bc130decdb0e87cf447956a67f5e103859d1b73d
denisrheault <rheault.denis@gmail.com>
2019-01-19 10:26:47 -0500

    moved linefollower to subsystems

--- src/main/java/frc/subsystems/LineFollower.java
+++ src/main/java/frc/subsystems/LineFollower.java
@@ -1,11 +1,23 @@
-package frc.utils;
+package frc.subsystems;
 
 import edu.wpi.first.wpilibj.DigitalInput;
 
-public class LineFollower
-{
+public class LineFollower extends Subsystems {
+    DigitalInput rightColorSensor = new DigitalInput(1);
+    DigitalInput leftColorSensor = new DigitalInput(2);
+    boolean rightColorSensorValue = rightColorSensor.get();
+    boolean leftColorSensorValue = leftColorSensor.get(); 
     public LineFollower()
     {
-        DigitalInput Sensor = new DigitalInput(1);
+        if(rightColorSensorValue = true) {
+            
+        }
+        if(leftColorSensorValue = true) {
+
+        }
+
+    }
+    public static void start() {
+        if(leftColorSensorValue = false)
     }
 }
