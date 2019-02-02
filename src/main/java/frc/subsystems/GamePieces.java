1f9468d37ed3fd2deb9edea84a9b7412283f5087
Aaron Jumarang <kapj.jumarang@gmail.com>
2019-02-02 15:26:51 -0500

    GamePieces with constants

--- src/main/java/frc/subsystems/GamePieces.java
+++ src/main/java/frc/subsystems/GamePieces.java
@@ -1,11 +1,17 @@
 package frc.subsystems;
 
+import frc.utils.Constants;
+import frc.utils.Constants.*;
+
 import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.PWM;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 
@@ -13,23 +19,28 @@ import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 public class GamePieces extends Subsystems {
-    private static CANSparkMax arm;
-    private static VictorSPX hatch;
-    private static VictorSPX cargo;
-    private static Solenoid hatchPush;
+    DoubleSolenoid hatch = new DoubleSolenoid(Constants.PCM_PISTON_PUSH, Constants.PCM_PISTON_PUSH);
+    DoubleSolenoid finger = new DoubleSolenoid(Constants.PCM_FINGER_HOLD, Constants.PCM_PISTON_RETRACT);
+    VictorSPX cargo = new VictorSPX(Constants.CAN_WHEEL_INTAKE);
+
+    public GamePieces() {
+        System.out.println("Game Piece Subsystem activated! ");
+    }
 
     public void wheelIntake() { 
-
+        cargo.set(ControlMode.PercentOutput, 1);
     }
-    public void wheelShoot() {
-        
+    public void wheelShoot() { 
+        cargo.set(ControlMode.PercentOutput, -1);
     }
 
     //HATCH CONTROL
     public void hatchPull() {
-        
+        finger.set(Value.kForward);
     }
     public void hatchPush() {
-
+        finger.set(Value.kReverse);
+        hatch.set(Value.kForward);
+        hatch.set(Value.kReverse);
     }
 }
