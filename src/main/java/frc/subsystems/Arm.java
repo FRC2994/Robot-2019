3e468a52993552f3ddf8f3d0035ab661520f0205
Grayden Hibbert <graydenhibbert@hotmail.com>
2019-02-02 10:28:56 -0500

    Created arm.java

---
+++ src/main/java/frc/subsystems/Arm.java
@@ -0,0 +1,12 @@
+package frc.subsystems;
+
+import edu.wpi.first.wpilibj.Solenoid;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+
+import com.revrobotics.CANSparkMax;
+
+public class Arm extends Subsystem {
+    
+
+}
